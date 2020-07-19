ff4ff090dfd9d3aa63b31684504a402bdf4ce9d7
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-07-19 20:19:47 +0100

    Fix failing Jest tests

--- webapp/src/games/Game.test.js
+++ webapp/src/games/Game.test.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {configure, shallow} from 'enzyme';
 import Games from "./Games";
 import Adapter from 'enzyme-adapter-react-16';
+import configureStore from "redux-mock-store";
+import {Provider} from "react-redux";
 
-configure({ adapter: new Adapter() })
+configure({adapter: new Adapter()})
+
+const mockStore = configureStore([]);
+let store = mockStore({});
 
 test('Matches Games snapshot', () => {
-    const tree = shallow(<Games/>);
+    const tree = shallow(<Provider store={store}><Games/></Provider>);
     expect(tree).toMatchSnapshot()
 });
