437eb935deb0709969fca0b0f23107f80a9cd38c
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-08-27 19:24:15 +0100

    Fix websocket config

--- server/src/main/java/com/cheung/tim/server/WebSocketConfig.java
+++ server/src/main/java/com/cheung/tim/server/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.cheung.tim.server;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
@@ -13,5 +14,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").withSockJS();
     }
 }
