a870e7e90083d03165d1cbe5fb1b8f8b60777dae
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-09-02 11:37:21 +0100

    Cleanup unused functions and settings

--- server/src/main/java/com/cheung/tim/server/ApplicationConfig.java
+++ server/src/main/java/com/cheung/tim/server/ApplicationConfig.java
@@ -19,8 +19,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE").allowCredentials(true);
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE").allowCredentials(true);
     }
 
     @Override
--- webapp/src/common/Util.js
+++ webapp/src/common/Util.js
@@ -5,13 +5,3 @@ export function isBlank(str) {
 }
 
 export const flushPromises = () => new Promise(resolve => setTimeout(resolve))
-
-function wait(amount = 0) {
-    return new Promise(resolve => setTimeout(resolve, amount));
-}
-
-export async function actWait(amount = 0) {
-    await act(async () => {
-        await wait(amount);
-    });
-}
